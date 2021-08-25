523c1f335a3f6719828be12a4b3b9de82f99ca33
hanbing.liang <hanbing.liang@thoughtworks.com>
2021-08-25 13:51:23 +0800

    feat:establish learning project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpSelfLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
