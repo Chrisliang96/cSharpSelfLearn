c8e37c69dfe261e2ee3ae319fc30fe22cb5cf207
hanbing.liang <hanbing.liang@thoughtworks.com>
2021-08-25 14:13:55 +0800

    feat: basic grammer and read or writeline

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # cSharpSelfLearn
+# month plan
+## 1、初步上手c#，尽快掌握c#程序结构、句法类型、面向对象等基础特性。
+	1.1：通过github开源项目、快速掌握上述c#语言基础特性。
+	1.2：针对期中有疑问的点，通过microsoft官方文档，查找问题解决办法。
+	参考资料：git@github.com:benperk/BeginningCSharp7.git
+			https://docs.microsoft.com/en/dotnet/csharp/tour-of-csharp/
+## 2、在掌握c#基础语、句法后，了解c#高级属性，目前优先级较高的有c#委托(Delegate)、反射(Reflection)等
+	2.1：了解c#异常处理、正则表达式使用。
+	2.2：了解c#和数据库的操作。
+	参考资料：https://docs.microsoft.com/en-us/dotnet/csharp/
+			https://www.runoob.com/csharp/csharp-tutorial.html
+## 3、根据今早zoom会议中的模型，了解c#中Entity Framework Core相关知识
+	参考资料：https://docs.microsoft.com/en-us/ef/
+预计session时间
+Sep. 10th 之前
---
+++ basiclanguage/basicGrammer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicGrammer{
+    class program{
+            
+        static void Main(string[] args){
+            double firstNumber = 2L;
+            double secondNumber = 3L;
+            string userName = "lhb";
+            Console.WriteLine("Enter your name:");
+            Console.WriteLine($"youname {userName}");
+            
+            Console.WriteLine("Now give me a number:");
+            
+            Console.WriteLine("Now give me another number:");
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is " +
+                          $"{firstNumber + secondNumber}.");
+            Console.WriteLine($"The result of subtracting {secondNumber} from " +
+                          $"{firstNumber} is {firstNumber - secondNumber}.");
+            Console.WriteLine($"The product of {firstNumber} and {secondNumber} " +
+                          $"is {firstNumber * secondNumber}.");
+            Console.WriteLine($"The result of dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber / secondNumber}.");
+            Console.WriteLine($"The remainder after dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber % secondNumber}.");
+        }
+
+    }
+
+}
