d8f6ff19855126bd8294acac5c3a77d0e408ddbf
hanbing.liang <hanbing.liang@thoughtworks.com>
2021-09-07 10:11:39 +0800

    feat :think about Boxing game contain and design armor

---
+++ BoxingGame/Combat/Armor/ArmorEfficent.cs
@@ -0,0 +1,10 @@
+namespace cSharpSelfLearn.BoxingGame.Combat.Armor
+{
+    public class ArmorEfficent : IArmor
+    {
+        public float GetArmor()
+        {
+            return 0.6f;
+        }
+    }
+}
